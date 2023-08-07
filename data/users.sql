70b0af8e1220ba713639dea4fe7819e3831751ce
Đặng Ngọc Quân <dangngocquan104@gmail.com>
2023-08-07 14:38:20 +0700

    Fill sample data for table users

---
+++ data/users.sql
@@ -0,0 +1,12 @@
+insert into `users` (`username`, `password`, `first_name`, `last_name`, `gender`, `email`, `phone_number`, `country_id`, `city_id`, `address_details`)
+values
+("usernamebot01", "passwordbot01", "Fisrtname01", "Lastname01", "male", "email01@gmail.com", "0356020111", 192, 3765, "address exapmle 01"),
+("usernamebot02", "passwordbot02", "Fisrtname02", "Lastname02", "male", "email01@gmail.com", "0356020112", 192, 3765, "address exapmle 02"),
+("usernamebot03", "passwordbot03", "Fisrtname03", "Lastname03", "female", "email01@gmail.com", "0356020113", 192, 3765, "address exapmle 03"),
+("usernamebot04", "passwordbot04", "Fisrtname04", "Lastname04", "other", "email01@gmail.com", "0356020114", 192, 3765, "address exapmle 04"),
+("usernamebot05", "passwordbot05", "Fisrtname05", "Lastname05", "other", "email01@gmail.com", "0356020115", 192, 3765, "address exapmle 05"),
+("usernamebot06", "passwordbot06", "Fisrtname06", "Lastname06", "female", "email01@gmail.com", "0356020116", 192, 3765, "address exapmle 06"),
+("usernamebot07", "passwordbot07", "Fisrtname07", "Lastname07", "female", "email01@gmail.com", "0356020117", 192, 3765, "address exapmle 07"),
+("usernamebot08", "passwordbot08", "Fisrtname08", "Lastname08", "male", "email01@gmail.com", "0356020118", 192, 3765, "address exapmle 08"),
+("usernamebot09", "passwordbot09", "Fisrtname09", "Lastname09", "female", "email01@gmail.com", "0356020119", 192, 3765, "address exapmle 09"),
+("usernamebot10", "passwordbot10", "Fisrtname10", "Lastname10", "male", "email01@gmail.com", "0356020120", 192, 3765, "address exapmle 10");
