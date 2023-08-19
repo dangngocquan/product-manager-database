7f2af868a1e5467f1c5353991bdeef40405fde09
Đặng Ngọc Quân <dangngocquan104@gmail.com>
2023-08-19 07:22:32 +0700

    Change to PostgreSQL

--- structure/constraints.sql
+++ structure/constraints.sql
@@ -1,62 +1,62 @@
 -- ADD CONSTRAINT FOR TABLES
 
--- Add constraint for table `clients`
-alter table `clients` add constraint `fk_clients_accounts` foreign key (`account_id`) references `accounts`(`id`);
+-- Add constraint for table "clients"
+alter table "clients" add constraint "fk_clients_accounts" foreign key ("account_id") references "accounts"("id");
 
--- Add constraint for table `history_logins`
-alter table `history_logins` add constraint `fk_historyLogins_accounts` foreign key (`account_id`) references `accounts`(`id`);
+-- Add constraint for table "history_logins"
+alter table "history_logins" add constraint "fk_historyLogins_accounts" foreign key ("account_id") references "accounts"("id");
 
--- Add constraint for table `districts`
-alter table `districts` add constraint `fk_districts_cities` foreign key (`city_id`) references `cities`(`id`);
+-- Add constraint for table "districts"
+alter table "districts" add constraint "fk_districts_cities" foreign key ("city_id") references "cities"("id");
 
--- Add constraint for table `wards`
-alter table `wards` add constraint `fk_wards_districts` foreign key (`district_id`) references `districts`(`id`);
+-- Add constraint for table "wards"
+alter table "wards" add constraint "fk_wards_districts" foreign key ("district_id") references "districts"("id");
 
--- Add constraint for table `addresses`
-alter table `addresses` add constraint `fk_addresses_clients` foreign key (`user_id`) references `clients`(`id`);
-alter table `addresses` add constraint `fk_addresses_cities` foreign key (`city_id`) references `cities`(`id`);
-alter table `addresses` add constraint `fk_addresses_districts` foreign key (`district_id`) references `districts`(`id`);
-alter table `addresses` add constraint `fk_addresses_wards` foreign key (`ward_id`) references `wards`(`id`);
+-- Add constraint for table "addresses"
+alter table "addresses" add constraint "fk_addresses_clients" foreign key ("user_id") references "clients"("id");
+alter table "addresses" add constraint "fk_addresses_cities" foreign key ("city_id") references "cities"("id");
+alter table "addresses" add constraint "fk_addresses_districts" foreign key ("district_id") references "districts"("id");
+alter table "addresses" add constraint "fk_addresses_wards" foreign key ("ward_id") references "wards"("id");
 
--- Add constraints for table `user_notifications`
-alter table `user_notifications` add constraint `fk_userNotifications_clients` foreign key (`user_id`) references `clients`(`id`);
+-- Add constraints for table "user_notifications"
+alter table "user_notifications" add constraint "fk_userNotifications_clients" foreign key ("user_id") references "clients"("id");
 
--- Add constraints for table `history_searchs`
-alter table `history_searchs` add constraint `fk_historySearchs_clients` foreign key (`user_id`) references `clients`(`id`);
+-- Add constraints for table "history_searchs"
+alter table "history_searchs" add constraint "fk_historySearchs_clients" foreign key ("user_id") references "clients"("id");
 
--- Add constraints for table `shops`
-alter table `shops` add constraint `fk_shops_accounts` foreign key (`owner_id`) references `accounts`(`id`);
+-- Add constraints for table "shops"
+alter table "shops" add constraint "fk_shops_accounts" foreign key ("owner_id") references "accounts"("id");
 
--- Add constraints for table `favourite_products`
-alter table `favourite_products` add constraint `fk_favouriteProducts_clients` foreign key (`user_id`) references `clients`(`id`);
-alter table `favourite_products` add constraint `fk_favouriteProducts_products` foreign key (`product_id`) references `products`(`id`);
+-- Add constraints for table "favourite_products"
+alter table "favourite_products" add constraint "fk_favouriteProducts_clients" foreign key ("user_id") references "clients"("id");
+alter table "favourite_products" add constraint "fk_favouriteProducts_products" foreign key ("product_id") references "products"("id");
 
--- Add constraints for table `carts`
-alter table `carts` add constraint `fk_carts_clients` foreign key (`owner_id`) references `clients`(`id`);
+-- Add constraints for table "carts"
+alter table "carts" add constraint "fk_carts_clients" foreign key ("owner_id") references "clients"("id");
 
--- Add constraints for table `followers_of_users`
-alter table `followers_of_users` add constraint `fk_followersOfUsers_clients1` foreign key (`user_id`) references `clients`(`id`);
-alter table `followers_of_users` add constraint `fk_followersOfUsers_clients2` foreign key (`follower_id`) references `clients`(`id`);
+-- Add constraints for table "followers_of_users"
+alter table "followers_of_users" add constraint "fk_followersOfUsers_clients1" foreign key ("user_id") references "clients"("id");
+alter table "followers_of_users" add constraint "fk_followersOfUsers_clients2" foreign key ("follower_id") references "clients"("id");
 
--- Add constraint for table `products_of_carts`
-alter table `products_of_carts` add constraint `fk_productsOfCarts_carts` foreign key (`cart_id`) references `carts`(`id`);
+-- Add constraint for table "products_of_carts"
+alter table "products_of_carts" add constraint "fk_productsOfCarts_carts" foreign key ("cart_id") references "carts"("id");
 
--- Add constraint for table `products`
-alter table `products` add constraint `fk_products_shops` foreign key (`shop_id`) references `shops`(`id`);
+-- Add constraint for table "products"
+alter table "products" add constraint "fk_products_shops" foreign key ("shop_id") references "shops"("id");
 
--- Add constraint for table `shop_notifications`
-alter table `shop_notifications` add constraint `fk_shopNotifications_shops` foreign key (`shop_id`) references `shops`(`id`);
+-- Add constraint for table "shop_notifications"
+alter table "shop_notifications" add constraint "fk_shopNotifications_shops" foreign key ("shop_id") references "shops"("id");
 
--- Add constraint for table `followers_of_shops`
-alter table `followers_of_shops` add constraint `fk_folowersOfShops_shops` foreign key (`shop_id`) references `shops`(`id`);
-alter table `followers_of_shops` add constraint `fk_folowersOfShops_clients` foreign key (`follower_id`) references `clients`(`id`);
+-- Add constraint for table "followers_of_shops"
+alter table "followers_of_shops" add constraint "fk_folowersOfShops_shops" foreign key ("shop_id") references "shops"("id");
+alter table "followers_of_shops" add constraint "fk_folowersOfShops_clients" foreign key ("follower_id") references "clients"("id");
 
--- Add constraint for table `orders`
-alter table `orders` add constraint `fk_orders_addresses` foreign key (`receiver_id`) references `addresses`(`id`);
-alter table `orders` add constraint `fk_orders_productVariations` foreign key (`product_variation_id`) references `product_variations` (`id`);
-alter table `orders` add constraint `fk_orders_deliveryCenters` foreign key (`delivery_center_id`) references `delivery_centers` (`id`);
-alter table `orders` add constraint `fk_orders_orderProcesses` foreign key (`process_id`) references `order_processes`(`id`);
-alter table `orders` add constraint `fk_orders_orderPaymentsInfo` foreign key (`payment_info_id`) references `order_payments_info` (`id`);
+-- Add constraint for table "orders"
+alter table "orders" add constraint "fk_orders_addresses" foreign key ("receiver_id") references "addresses"("id");
+alter table "orders" add constraint "fk_orders_productVariations" foreign key ("product_variation_id") references "product_variations" ("id");
+alter table "orders" add constraint "fk_orders_deliveryCenters" foreign key ("delivery_center_id") references "delivery_centers" ("id");
+alter table "orders" add constraint "fk_orders_orderProcesses" foreign key ("process_id") references "order_processes"("id");
+alter table "orders" add constraint "fk_orders_orderPaymentsInfo" foreign key ("payment_info_id") references "order_payments_info" ("id");
 
--- Add constraints for table `order_payments_info`
-alter table `order_payments_info` add constraint `fk_orderPaymentsInfo_paymentMethods` foreign key (`payment_method_id`) references `payment_methods`(`id`);
+-- Add constraints for table "order_payments_info"
+alter table "order_payments_info" add constraint "fk_orderPaymentsInfo_paymentMethods" foreign key ("payment_method_id") references "payment_methods"("id");
--- structure/tables.sql
+++ structure/tables.sql
@@ -1,241 +1,252 @@
 -- CREATE TABLES 
 
--- Create table `accounts`
-create table `accounts` (
-    `id` bigint unsigned not null auto_increment primary key,
-    `username` varchar(255) not null,
-    `password` varchar(255) not null,
-    `status` enum("normal", "banned", "deleted") not null default "normal",
-    `type` enum("user", "admin") not null default "user",
-    `time_registered` datetime not null default current_timestamp
+-- Create type enum
+CREATE TYPE enum_status_1 AS enum('normal', 'banned', 'deleted');
+CREATE TYPE enum_status_2 AS enum('normal', 'deleted');
+CREATE TYPE enum_status_3 AS enum('normal', 'unable', 'deleted');
+CREATE TYPE enum_status_4 AS enum('normal', 'hidden', 'deleted');
+CREATE TYPE enum_status_5 AS enum('cancelled', 'ordered', 'prepared', 'completed');
+CREATE TYPE enum_types_1 AS enum('user', 'admin');
+CREATE TYPE enum_gender_1 AS enum('male', 'female', 'other');
+CREATE TYPE enum_0_1 AS enum('0', '1');
+
+-- CREATE TABLES 
+
+-- Create table "accounts"
+create table "accounts" (
+    "id" bigserial not null primary key,
+    "username" varchar(255) not null,
+    "password" varchar(255) not null,
+    "status" enum_status_1 not null default 'normal',
+    "type" enum_types_1 not null default 'user',
+    "time_registered" timestamp not null default current_timestamp
 );
 
--- Create table `clients`
-create table `clients` (
-    `id` bigint unsigned not null auto_increment primary key,
-    `account_id` bigint unsigned not null,
-    `nickname` varchar(255) not null,
-    `email` varchar(255),
-    `phone_number` varchar(255),
-    `gender` enum("male", "female", "other"),
-    `birthday` datetime not null default current_timestamp,
-    `portrait` text
+-- Create table "clients"
+create table "clients" (
+    "id" bigserial not null primary key,
+    "account_id" bigint not null,
+    "nickname" varchar(255) not null,
+    "email" varchar(255),
+    "phone_number" varchar(255),
+    "gender" enum_gender_1,
+    "birthday" timestamp not null default current_timestamp,
+    "portrait" text
 );
 
--- Create table `cities`
-create table `cities` (
-    `id` bigint unsigned not null auto_increment primary key,
-    `name` varchar(255) not null,
-    `status` enum("normal", "deleted") not null default "normal"
+-- Create table "cities"
+create table "cities" (
+    "id" bigserial not null primary key,
+    "name" varchar(255) not null,
+    "status" enum_status_2 not null default 'normal'
 );
 
--- Create table `districts`
-create table `districts` (
-    `id` bigint unsigned not null auto_increment primary key,
-    `name` varchar(255) not null,
-    `city_id` bigint unsigned not null,
-    `status` enum("normal", "deleted") not null default "normal"
+-- Create table "districts"
+create table "districts" (
+    "id" bigserial not null primary key,
+    "name" varchar(255) not null,
+    "city_id" bigint not null,
+    "status" enum_status_2 not null default 'normal'
 );
 
--- Create table `wards`
-create table `wards` (
-    `id` bigint unsigned not null auto_increment primary key,
-    `name` varchar(255) not null,
-    `district_id` bigint unsigned not null,
-    `status` enum("normal", "deleted") not null default "normal"
+-- Create table "wards"
+create table "wards" (
+    "id" bigserial not null primary key,
+    "name" varchar(255) not null,
+    "district_id" bigint not null,
+    "status" enum_status_2 not null default 'normal'
 );
 
--- Create table `addresses`
-create table `addresses` (
-    `id` bigint unsigned not null auto_increment primary key,
-    `user_id` bigint unsigned not null,
-    `phone_number` varchar(255) not null,
-    `ward_id` bigint unsigned not null,
-    `district_id` bigint unsigned not null,
-    `city_id` bigint unsigned not null,
-    `address` text,
-    `is_deliver_address` enum("0", "1") not null,
-    `time_created` datetime not null default current_timestamp,
-    `status` enum("normal", "deleted") not null default "normal"
+-- Create table "addresses"
+create table "addresses" (
+    "id" bigserial not null primary key,
+    "user_id" bigint not null,
+    "phone_number" varchar(255) not null,
+    "ward_id" bigint not null,
+    "district_id" bigint not null,
+    "city_id" bigint not null,
+    "address" text,
+    "is_deliver_address" enum_0_1 not null,
+    "time_created" timestamp not null default current_timestamp,
+    "status" enum_status_2 not null default 'normal'
 );
 
--- Create table `history_logins`
-create table `history_logins` (
-    `id` bigint unsigned not null auto_increment primary key,
-    `account_id` bigint unsigned not null,
-    `time` datetime not null default current_timestamp
+-- Create table "history_logins"
+create table "history_logins" (
+    "id" bigserial not null primary key,
+    "account_id" bigint not null,
+    "time" timestamp not null default current_timestamp
 );
 
--- Create table `user_notifications`
-create table `user_notifications` (
-    `id` bigint unsigned not null auto_increment primary key,
-    `user_id` bigint unsigned not null,
-    `title` varchar(255) not null,
-    `description` text,
-    `details_info` json not null,
-    `time_created` datetime not null default current_timestamp,
-    `is_seen` enum("0", "1") not null default "0"
+-- Create table "user_notifications"
+create table "user_notifications" (
+    "id" bigserial not null primary key,
+    "user_id" bigint not null,
+    "title" varchar(255) not null,
+    "description" text,
+    "details_info" json not null,
+    "time_created" timestamp not null default current_timestamp,
+    "is_seen" enum_0_1 not null default '0'
 );
 
--- Create table `history_searchs`
-create table `history_searchs` (
-    `id` bigint unsigned not null auto_increment primary key,
-    `user_id` bigint unsigned not null,
-    `content` varchar(255) not null,
-    `time_searched` datetime not null default current_timestamp
+-- Create table "history_searchs"
+create table "history_searchs" (
+    "id" bigserial not null primary key,
+    "user_id" bigint not null,
+    "content" varchar(255) not null,
+    "time_searched" timestamp not null default current_timestamp
 );
 
--- Create table `shops`
-create table `shops` (
-    `id` bigint unsigned not null auto_increment primary key,
-    `owner_id` bigint unsigned not null,
-    `username` varchar(255) not null,
-    `brand_name` varchar(255) not null,
-    `time_created` datetime not null default current_timestamp,
-    `logo` text,
-    `status` enum("normal", "banned", "deleted") not null default "normal"
+-- Create table "shops"
+create table "shops" (
+    "id" bigserial not null primary key,
+    "owner_id" bigint not null,
+    "username" varchar(255) not null,
+    "brand_name" varchar(255) not null,
+    "time_created" timestamp not null default current_timestamp,
+    "logo" text,
+    "status" enum_status_1 not null default 'normal'
 );
 
--- Create table `carts`
-create table `carts` (
-    `id` bigint unsigned not null auto_increment primary key,
-    `owner_id` bigint unsigned not null,
-    `time_created` datetime not null default current_timestamp
+-- Create table "carts"
+create table "carts" (
+    "id" bigserial not null primary key,
+    "owner_id" bigint not null,
+    "time_created" timestamp not null default current_timestamp
 );
 
--- Create table `followers_of_users`
-create table `followers_of_users` (
-    `id` bigint unsigned not null auto_increment primary key,
-    `user_id` bigint unsigned not null,
-    `follower_id` bigint unsigned not null,
-    `time_followed` datetime not null default current_timestamp
+-- Create table "followers_of_users"
+create table "followers_of_users" (
+    "id" bigserial not null primary key,
+    "user_id" bigint not null,
+    "follower_id" bigint not null,
+    "time_followed" timestamp not null default current_timestamp
 );
 
--- Create table `followers_of_shops`
-create table `followers_of_shops` (
-    `id` bigint unsigned not null auto_increment primary key,
-    `shop_id` bigint unsigned not null,
-    `follower_id` bigint unsigned not null,
-    `time_followed` datetime not null default current_timestamp
+-- Create table "followers_of_shops"
+create table "followers_of_shops" (
+    "id" bigserial not null primary key,
+    "shop_id" bigint not null,
+    "follower_id" bigint not null,
+    "time_followed" timestamp not null default current_timestamp
 );
 
--- Create table `shop_notifications`
-create table `shop_notifications` (
-    `id` bigint unsigned not null auto_increment primary key,
-    `shop_id` bigint unsigned not null,
-    `title` varchar(255) not null,
-    `description` varchar(255),
-    `details_info` json not null,
-    `time_created` datetime not null default current_timestamp,
-    `is_seen` enum("0", "1") not null default "0"
+-- Create table "shop_notifications"
+create table "shop_notifications" (
+    "id" bigserial not null primary key,
+    "shop_id" bigint not null,
+    "title" varchar(255) not null,
+    "description" varchar(255),
+    "details_info" json not null,
+    "time_created" timestamp not null default current_timestamp,
+    "is_seen" enum_0_1 not null default '0'
 );
 
--- Create table `products`
-create table `products` (
-    `id` bigint unsigned not null auto_increment primary key,
-    `shop_id` bigint unsigned not null,
-    `name` varchar(255) not null,
-    `image` text,
-    `price` bigint unsigned not null,
-    `currency` varchar(255) not null default "VND",
-    `stock` bigint unsigned not null,
-    `time_added` timestamp not null default current_timestamp,
-    `description` text,
-    `status` enum("normal", "deleted") not null default "normal"
+-- Create table "products"
+create table "products" (
+    "id" bigserial not null primary key,
+    "shop_id" bigint not null,
+    "name" varchar(255) not null,
+    "image" text,
+    "price" bigint not null,
+    "currency" varchar(255) not null default 'VND',
+    "stock" bigint not null,
+    "time_added" timestamp not null default current_timestamp,
+    "description" text,
+    "status" enum_status_2 not null default 'normal'
 );
 
--- Create table `favourite_products`
-create table `favourite_products` (
-    `id` bigint unsigned not null auto_increment primary key,
-    `product_id` bigint unsigned not null,
-    `user_id` bigint unsigned not null,
-    `time_liked` timestamp not null default current_timestamp
+-- Create table "favourite_products"
+create table "favourite_products" (
+    "id" bigserial not null primary key,
+    "product_id" bigint not null,
+    "user_id" bigint not null,
+    "time_liked" timestamp not null default current_timestamp
 );
 
--- Create table `products_of_carts`
-create table `products_of_carts` (
-    `id` bigint unsigned not null auto_increment primary key,
-    `product_id` bigint unsigned not null,
-    `cart_id` bigint unsigned not null,
-    `time_added` timestamp not null default current_timestamp
+-- Create table "products_of_carts"
+create table "products_of_carts" (
+    "id" bigserial not null primary key,
+    "product_id" bigint not null,
+    "cart_id" bigint not null,
+    "time_added" timestamp not null default current_timestamp
 );
 
--- Create table `categories`
-create table `categories` (
-    `id` bigint unsigned not null auto_increment primary key,
-    `name` varchar(255) not null,
-    `image` text,
-    `level` int not null,
-    `parent_category_id` bigint unsigned not null,
-    `time_added` timestamp not null default current_timestamp,
-    `status` enum("normal", "hidden", "deleted") not null default "normal"
+-- Create table "categories"
+create table "categories" (
+    "id" bigserial not null primary key,
+    "name" varchar(255) not null,
+    "image" text,
+    "level" int not null,
+    "parent_category_id" bigint not null,
+    "time_added" timestamp not null default current_timestamp,
+    "status" enum_status_4 not null default 'normal'
 );
 
--- Create table `products_of_categories`
-create table `products_of_categories` (
-    `id` bigint unsigned not null auto_increment primary key,
-    `category_id` bigint unsigned not null,
-    `product_id` bigint unsigned not null
+-- Create table "products_of_categories"
+create table "products_of_categories" (
+    "id" bigserial not null primary key,
+    "category_id" bigint not null,
+    "product_id" bigint not null
 );
 
--- Create table `product_images`
-create table `product_images` (
-    `id` bigint unsigned not null auto_increment primary key,
-    `product_id` bigint unsigned not null,
-    `image` text not null,
-    `time_added` datetime not null default current_timestamp
+-- Create table "product_images"
+create table "product_images" (
+    "id" bigserial not null primary key,
+    "product_id" bigint not null,
+    "image" text not null,
+    "time_added" timestamp not null default current_timestamp
 );
 
--- Create table `product_variations`
-create table `product_variations` (
-    `id` bigint unsigned not null auto_increment primary key,
-    `product_id` bigint unsigned not null,
-    `attributes` json not null,
-    `price` bigint unsigned not null,
-    `status` enum("normal", "deleted") not null default "normal"
+-- Create table "product_variations"
+create table "product_variations" (
+    "id" bigserial not null primary key,
+    "product_id" bigint not null,
+    "attributes" json not null,
+    "price" bigint not null,
+    "status" enum_status_2 not null default 'normal'
 );
 
--- Create table `orders`
-create table `orders` (
-    `id` bigint unsigned not null auto_increment primary key,
-    `product_variation_id` bigint unsigned not null,
-    `receiver_id` bigint unsigned not null,
-    `delivery_center_id` bigint unsigned not null,
-    `process_id` bigint unsigned not null,
-    `payment_info_id` bigint unsigned not null,
-    `status` enum("cancelled", "ordered", "prepared", "completed") not null default "ordered"
+-- Create table "orders"
+create table "orders" (
+    "id" bigserial not null primary key,
+    "product_variation_id" bigint not null,
+    "receiver_id" bigint not null,
+    "delivery_center_id" bigint not null,
+    "process_id" bigint not null,
+    "payment_info_id" bigint not null,
+    "status" enum_status_5 not null default 'ordered'
 );
 
--- Create table `delivery_centers`
-create table `delivery_centers` (
-    `id` bigint unsigned not null auto_increment primary key,
-    `name` varchar(255) not null,
-    `fee` bigint unsigned not null,
-    `status` enum("normal", "unable", "deleted") not null default "normal"
+-- Create table "delivery_centers"
+create table "delivery_centers" (
+    "id" bigserial not null primary key,
+    "name" varchar(255) not null,
+    "fee" bigint not null,
+    "status" enum_status_3 not null default 'normal'
 );
 
--- Create table `order_payments_info`
-create table `order_payments_info` (
-    `id` bigint unsigned not null auto_increment primary key,
-    `product_price` bigint unsigned not null,
-    `quantity` bigint unsigned not null,
-    `deliver_fee` bigint unsigned not null,
-    `payment_method_id` bigint unsigned not null
+-- Create table "order_payments_info"
+create table "order_payments_info" (
+    "id" bigserial not null primary key,
+    "product_price" bigint not null,
+    "quantity" bigint not null,
+    "deliver_fee" bigint not null,
+    "payment_method_id" bigint not null
 );
 
--- Create table `payment_methods`
-create table `payment_methods` (
-    `id` bigint unsigned not null auto_increment primary key,
-    `name` varchar(255) not null,
-    `status` enum("normal", "unable", "deleted") not null default "normal"
+-- Create table "payment_methods"
+create table "payment_methods" (
+    "id" bigserial not null primary key,
+    "name" varchar(255) not null,
+    "status" enum_status_3 not null default 'normal'
 );
 
--- Create table `order_processes`
-create table `order_processes` (
-    `id` bigint unsigned not null auto_increment primary key,
-    `time_ordered` timestamp,
-    `time_prepared` timestamp,
-    `time_completed` timestamp,
-    `time_cancelled` timestamp
+-- Create table "order_processes"
+create table "order_processes" (
+    "id" bigserial not null primary key,
+    "time_ordered" timestamp,
+    "time_prepared" timestamp,
+    "time_completed" timestamp,
+    "time_cancelled" timestamp
 );
-
